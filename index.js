0d61c080ae8856e2a9aca180ae384faf6ffb4589
danieltb92 <daniel.tunjano92@gmail.com>
2023-11-15 13:50:29 -0500

    website working on production OK

--- index.js
+++ index.js
@@ -1,38 +1,25 @@
 // import { getProjects } from "./services/notion"
 const express = require('express');
 const getProjects = require('./services/notion');
-const getPages = require('./services/pages');
+// const getPages = require('./services/pages');
 const PORT = process.env.PORT || 5000;
 
 const app = express()
 
 app.use(express.static('public'))
 
-// app.use(function (req, res, next) {
-//   res.setHeaders({
-//     'Content-Security-Policy': 'default-src 'none'; font-src 'self' https://example.com/fonts/;',
-//   });
-//   next();
-// });
+
 
 app.get('/projects', async (req, res) => {
   const projects = await getProjects()
   res.json(projects)
 })
 
-app.get('/rockTicket-app', async (req, res) => {
-  const fileName = 'rockTicked-app.html'
-  res.sendFile(path.join(__dirname, 'views', fileName))
+// app.get('/rockTicket-app', async (req, res) => {
+//   const fileName = 'rockTicked-app.html'
+//   res.sendFile(path.join(__dirname, 'views', fileName))
 
-})
-
-app.use(function (req, res, next) {
-  res.setHeaders({
-    'Content-Security-Policy': 'default-src 'none';
-     script - src "https://api.notion.com"; ',
-  });
-  next();
-});
+// })
 
 
 // app.get('/${projects.id}',async (req, res) =>{
