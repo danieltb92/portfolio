95f28a8813b8900378d31a7adebbbd7eb6c26c3f
danieltb92 <daniel.tunjano92@gmail.com>
2024-03-15 14:04:14 -0500

    update: page routers

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // import { getProjects } from "./services/notion"
 const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv').config();
 const getProjects = require('./services/notion');
 // const getPages = require('./services/pages');
 const PORT = process.env.PORT || 5000;
@@ -9,17 +11,60 @@ const app = express()
 app.use(express.static('public'))
 
 
+// Ruta URL del json de los proyectos de la API en Notion  //
 
 app.get('/projects', async (req, res) => {
   const projects = await getProjects()
   res.json(projects)
 })
 
-// app.get('/rockTicket-app', async (req, res) => {
-//   const fileName = 'rockTicked-app.html'
-//   res.sendFile(path.join(__dirname, 'views', fileName))
+// Ruta URL del landing page //
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+// Rutas URL de los proyectos //
+
+app.get('/rockTicket-app', (req, res) => {
+  const fileName = 'rockTicket-app.html'
+  res.sendFile(path.join(__dirname,'public/views/rockTicket-app',fileName))
+
+})
+
+app.get('/rockTicket-app-web', (req, res) => {
+  const fileName = 'rockTicket-app-web.html'
+  res.sendFile(path.join(__dirname, 'public/views/rockTicket-app-web', fileName))
+
+})
+
+app.get('/dashboard-contabilidad', (req, res) => {
+  const fileName = 'dashboard-contabilidad.html'
+  res.sendFile(path.join(__dirname, 'public/views/dashboard-contabilidad', fileName))
+
+})
+
+app.get('/superfresh', (req, res) => {
+  const fileName = 'superfresh.html'
+  res.sendFile(path.join(__dirname, 'public/views/superfresh', fileName))
+
+})
+
+app.get('/portfolio', (req, res) => {
+  const fileName = 'portfolio.html'
+  res.sendFile(path.join(__dirname, 'public/views/portfolio', fileName))
+
+})
+
+// app.get('/rockT', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/views/rockTicked-app/rockTicked-app.html')	)
 
 // })
+
+
+  // res.sendFile(path.join(__dirname, 'views', 'rockTicket-app', fileName))
+  // res.sendFile(path.join(__dirname, 'views', 'rockTicket-app', fileName))
+  // res.sendFile(path.join(__dirname, 'views', 'rockTicket-app', fileName))
 
 
 // app.get('/${projects.id}',async (req, res) =>{
@@ -35,5 +80,8 @@ app.get('/projects', async (req, res) => {
 
 
 
-app.listen(PORT, console.log('Server started on port  http://localhost:5000'));
+app.listen(PORT,
+  console.log(`Server started on port  http://localhost:${PORT}`)
+);
+
 
