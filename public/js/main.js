38072e49506bec8d57719c2677813902eb0930a4
danieltb92 <daniel.tunjano92@gmail.com>
2024-03-15 14:12:29 -0500

    update: main-dotenv v1

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,29 @@
-const projectsEl = document.querySelector('.grid-projects')
+const projectsEl = document.querySelector('.grid-projects');
+
+// const dotenv = require('dotenv');
+// const path = require('path');
+// const envPath = path.join(__dirname,'..','.env');
+// dotenv.config({ path: envPath });
+
+// if (process.env.NODE_ENV === 'development') { 
+//   console.log('development')
+//   require('dotenv').config();
+// }
+
 // const loadingEl = document.querySelector('#loading')
 // let loading = false
 
+// const PORT = process.env.PORT;
+// console.log (PORT)
+// console.log(process.env.API_URL)
+// console.log(process.env.NODE_ENV)
+
 const getProjectFromBackend = async () => {
 //   loading = true
-  // const res = await fetch('http://localhost:5000/projects')
-  const res = await fetch('https://daniel-tunjano.onrender.com/projects')
+  // const res = await fetch(`${process.env.API_URL}${PORT}/projects`)
+  // console.log(res)
+  // const res = await fetch('https://daniel-tunjano.onrender.com/projects')
+  const res = await fetch('http://localhost:5000/projects')
   const data = await res.json()
 //   loading = false
   return data
